1cdb59c53b5d70628b1f4ac87384af6293be2362
Chds154 <129257730+Chds154@users.noreply.github.com>
2023-03-29 08:50:26 +0200

    code alerts

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,5 +1,3 @@
-import sys 
-import os
 import yaml
 import flask
 
@@ -14,7 +12,7 @@ def index():
 
         
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
-class Person(object):
+class Person():
     def __init__(self, name):
         self.name = name
 
@@ -25,12 +23,11 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    import urllib{urllib_version} as urllib
     # Fetch and print the requested URL
  
     try: 
         http = urllib.PoolManager()
-        r = http.request('GET', url)
     except:
         print('Exception')
 
